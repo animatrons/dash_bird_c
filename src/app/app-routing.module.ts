8cbffd629c20a5a61025fea75b149e3f782e8bd4
Amin <medamineharwel@outlook.com>
2022-09-05 19:11:48 +0100

    added news module and sign in component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'sign-in',
+    component: SigninComponent,
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then(module => module.ProfileModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then(module => module.DashboardModule)
+  },
+  {
+    path: 'news',
+    loadChildren: () => import('./news/news.module').then(module => module.NewsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 import { HomeComponent } from './home/home.component';
+import { NewsModule } from './news/news.module';
+import { ProfileModule } from './profile/profile.module';
 import { SharedModule } from './shared/shared.module';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    ProfileModule,
+    DashboardModule,
+    NewsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
