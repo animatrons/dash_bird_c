196d1a3b8e8a8491b1be0985c0ad7b9ce468d485
Amin <medamineharwel@outlook.com>
2022-09-10 18:14:57 +0100

    login in auth.service returns obsrvable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'prefix'
+    pathMatch: 'full'
   },
   {
     path: 'home',
--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,33 +1,70 @@
 import { Inject, Injectable } from '@angular/core';
+import { Observable, of, retry, tap, timeout } from 'rxjs';
 import { RequestService } from '../http/request.service';
 import { User } from '../models/User';
 import { PersistenceService } from '../services/persistence.service';
 
+const REGISTERED_USERS_EXPL: User[] = [
+  {
+    id: '496',
+    email: 'osman@dot.com',
+    password: 'secret',
+    firstName: 'osman',
+    lastName: 'osmani'
+  },
+  {
+    id: '496',
+    email: 'hajib@dot.com',
+    password: 'secret',
+    firstName: 'hajib',
+    lastName: 'mahjoub'
+  }
+]
+
 @Injectable()
 export class AuthService {
   private loggedInUser: User | undefined;
-  private tokenKey = 'access_token';
+  private TOKEN_STORAGE_KEY = 'access_token';
   constructor(private httpService: RequestService,
     private persistenceService: PersistenceService) { }
 
-  login() {
-
+  login(email: string, password: string): Observable<User | undefined> {
+    const user = REGISTERED_USERS_EXPL.find(user =>
+      user.email.toLowerCase() === email.trim().toLowerCase()
+      && user.password === password)
+    //
+    let login$ = new Observable<User | undefined>()
+    login$ = of(user)
+      .pipe(
+        timeout(2000),
+        retry(1),
+        tap(v => {
+          this.loggedInUser = v;
+          if (v === undefined) {
+            throw new Error("User is invalid bro, check your credientials, are you even registered?");
+          }
+          // TODO: remove password from loggedInUser object
+          console.log('Got login result, let\'s see if it\'s a valid user: ', v);
+        }),
+      )
+    return login$;
   }
 
   logOut() {
+    this.loggedInUser = undefined;
     this.destroyToken();
   }
 
   setToken(token: string) {
-    this.persistenceService.set(this.tokenKey, token);
+    this.persistenceService.set(this.TOKEN_STORAGE_KEY, token);
   }
 
   getToken() {
-    return this.persistenceService.get(this.tokenKey);
+    return this.persistenceService.get(this.TOKEN_STORAGE_KEY);
   }
 
   private destroyToken() {
-    this.persistenceService.remove(this.tokenKey);
+    this.persistenceService.remove(this.TOKEN_STORAGE_KEY);
   }
 
   isUserLoggedIn() {
