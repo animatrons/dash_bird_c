f28515fee114db41cc6dfa9cbc719d180aab7832
Amin <medamineharwel@outlook.com>
2022-09-12 13:58:19 +0100

    finalized sign in component usin auth service

--- src/app/shared/standalone/signin/signin.component.ts
+++ src/app/shared/standalone/signin/signin.component.ts
@@ -3,55 +3,57 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { User } from 'src/app/core/models/User';
 import { AuthService } from 'src/app/core/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule
-  ],
-  providers: [
-    AuthService,
-  ],
+  imports: [CommonModule, FormsModule],
+  providers: [],
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.sass']
+  styleUrls: ['./signin.component.sass'],
 })
 export class SigninComponent implements OnInit {
   user: User = new User();
   isLoading: boolean = false;
   loggInError = false;
 
-  constructor
-    (
-      private authService: AuthService
-    ) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
+    this.initState();
   }
 
   onSubmit() {
     console.log('User credentials submitted: ', this.user);
-    this.isLoading = true;
-    this.authService.login(
-      this.user.email,
-      this.user.password
-    ).subscribe({
+    this.loadingState();
+    this.authService.login_(this.user.email, this.user.password).subscribe({
       next: (user) => {
         console.log('Welcome back sir/madame: ', user);
-        this.loggInError = false;
-        // this.messagingService.push(new MessageDTO(user, false));
+        this.initState();
+        this.router.navigate(['']);
       },
       error: (err) => {
-        console.error('Identity unregistered, invalid or rejcted', err);
-        this.loggInError = true;
-        // this.messagingService.push(new MessageDTO(err, true));
+        console.error('Identity unregistered, invalid or rejcted');
+        this.errorState();
       },
-      complete: () => {
-        this.user = new User();
-        this.isLoading = false;
-      }
-    })
+    });
+  }
+
+  initState() {
+    this.user = new User();
+    this.isLoading = false;
+    this.loggInError = false;
+  }
+
+  loadingState() {
+    this.isLoading = true;
+    this.loggInError = false;
+  }
+
+  errorState() {
+    this.loggInError = true;
+    this.isLoading = false;
   }
 
 }
