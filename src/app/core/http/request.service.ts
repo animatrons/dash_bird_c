8de22f9405eb6e89c0cdf824a7f56d1de7d9d845
Amin <medamineharwel@outlook.com>
2022-09-12 13:55:18 +0100

    fixed http request options in request service + a post.service

--- src/app/core/http/request.service.ts
+++ src/app/core/http/request.service.ts
@@ -1,49 +1,61 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { HttpOptionsDTO } from './HttpOptionsDTO';
+import { Observable } from 'rxjs';
+
+interface HttpOptions {
+  headers?: any;
+  params?: any;
+}
 
 @Injectable()
 export class RequestService {
   private baseUrl = environment.baseUrl;
   constructor(private httpClient: HttpClient) {}
 
-  get<T>(uri: string, requestDto?: HttpOptionsDTO) {
-    const options = this.getRequestOptions(requestDto);
+  get<T>(uri: string, requestDto?: HttpOptionsDTO): Observable<T> {
+    const options: HttpOptions = this.getRequestOptions(requestDto);
     return this.httpClient.get<T>(this.baseUrl + uri, options);
   }
 
   put<T>(uri: string, requestDto?: HttpOptionsDTO) {
-    const options = this.getRequestOptions(requestDto);
+    const options: HttpOptions = this.getRequestOptions(requestDto);
     const body = requestDto?.getBody;
     return this.httpClient.put<T>(this.baseUrl + uri, body, options);
   }
 
   post<T>(uri: string, requestDto?: HttpOptionsDTO) {
-    const options = this.getRequestOptions(requestDto);
+    const options: HttpOptions = this.getRequestOptions(requestDto);
     const body = requestDto?.getBody;
     return this.httpClient.post<T>(this.baseUrl + uri, body, options);
   }
 
   delete(uri: string, requestDto?: HttpOptionsDTO) {
-    const options = this.getRequestOptions(requestDto);
+    const options: HttpOptions = this.getRequestOptions(requestDto);
     return this.httpClient.delete(this.baseUrl + uri, options);
   }
 
-  private getRequestOptions(requestDto?: HttpOptionsDTO): any {
-    let options;
+  private getRequestOptions(requestDto?: HttpOptionsDTO): HttpOptions {
+    let options: HttpOptions = {};
     if (requestDto) {
       const paramsMap = requestDto.getParams;
       const headersMap = requestDto.getHeaders;
 
-      let params = new HttpParams();
+      let params: any = {};
       paramsMap.forEach((v, k) => {
-        params.set(k, v);
+        // if (!params.has(k)) {
+        //   params.append(k, v);
+        // } else params.set(k, v);
+        params[k] = v;
       });
       let headers = new HttpHeaders();
       headersMap.forEach((v, k) => {
-        headers.set(k, v);
+        if (!headers.has(k)) {
+          headers.append(k, v);
+        } else headers.set(k, v);
       });
-      options = { headers, params };
+      options = { headers: headers, params: params };
     }
     return options;
   }
