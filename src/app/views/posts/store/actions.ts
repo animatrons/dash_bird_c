a058a0f7dca98713c53a4e148766a53dbceb0ac4
Amin <medamineharwel@outlook.com>
2022-09-13 11:38:54 +0100

    init ngrx store and added actions for posts module

---
+++ src/app/views/posts/store/actions.ts
@@ -0,0 +1,13 @@
+import { createAction, props } from "@ngrx/store";
+import { TechnicalError } from "src/app/core/models/Error";
+import { PostInterface } from "../types/post.interface";
+
+export const getPosts = createAction('[Posts] Get Posts');
+export const getPostsSuccess = createAction(
+    '[Posts] Get Posts Success',
+    props<{posts: PostInterface[]}>()
+  );
+export const getPostsFailure = createAction(
+    '[Posts] Get Posts Failure',
+    props<{error: TechnicalError}>()
+  );
