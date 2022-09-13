7ece26a4842b041fc3a007dc7606cc874164b38e
Amin <medamineharwel@outlook.com>
2022-09-13 11:42:34 +0100

    dispatched actions in posts component to trigger the effects added + complete ngrx setup in root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 import { FormsModule } from '@angular/forms';
-import { HomeModule } from './views/home/home.module';
 import { ViewModule } from './views/view.module';
-// import { HomeModule } from './views/home_/home.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,16 @@ import { ViewModule } from './views/view.module';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    ViewModule
+    ViewModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot(),
+    StoreDevtoolsModule.instrument(
+      {
+        maxAge: 25, // Retains last 25 states
+        logOnly: environment.production, // Restrict extension to log-only mode
+        autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/posts/posts.component.ts
+++ src/app/views/posts/posts.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppStateInterface } from 'src/app/types/app-state.interface';
+import * as PostsActions from "./store/actions";
+import { isLoadingSelector } from './store/selectors';
 
 @Component({
   selector: 'app-posts',
@@ -6,10 +11,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.sass']
 })
 export class PostsComponent implements OnInit {
+  isLoading$: Observable<boolean>;
 
-  constructor() { }
+  constructor(private store: Store<AppStateInterface>) {
+    this.isLoading$ = this.store.pipe<boolean>(select(isLoadingSelector));
+  }
 
   ngOnInit(): void {
+    // dispatch the Get Posts action
+    this.store.dispatch(PostsActions.getPosts());
   }
 
 }
